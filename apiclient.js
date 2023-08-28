6b9af8a96630534342d74368f855612d147f6988
Volumio <info@volumio.org>
2023-08-28 18:50:23 +0200

    Add api client

---
+++ apiclient.js
@@ -0,0 +1,10 @@
+var superagent = require('superagent');
+
+var apiEndpoint = 'http://127.0.0.1:9876';
+
+
+superagent.get('http://127.0.0.1:9876/status')
+    .accept('application/json')
+    .then((results) => {
+        console.log(results.body);
+    })
