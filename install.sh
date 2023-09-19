4051643ff28d5c789f26cb515093a4a07796e2a1
Volumio <info@volumio.org>
2023-09-19 23:34:23 +0200

    Install plugin

--- install.sh
+++ install.sh
@@ -4,21 +4,20 @@ echo "Installing Go-librespot"
 
 ARCH=$(cat /etc/os-release | grep ^VOLUMIO_ARCH | tr -d 'VOLUMIO_ARCH="')
 
-echo "Copying appropriate Go-librespot Daemon"
-#cp ${DESTPATH}/bin/${ARCH}/streaming-daemon /bin/streaming-daemon
-#cp ${DESTPATH}/bin/${ARCH}/version_hash ${DESTPATH}/version_hash
-#chmod a+x /bin/streaming-daemon
+DAEMON_BASE_URL=https://repo.volumio.org/Packages/go-librespot-daemon/go-librespot-daemon
+DAEMON_DOWNLOAD_URL=$DAEMON_BASE_URL-$ARCH
 
-## TODO: Finish with compiled daemon
+# TODO DOWNLOAD FROM RELEASES URL
+echo "Dowloading daemon"
+wget $DAEMON_DOWNLOAD_URL -O /usr/bin/go-librespot-daemon
+chmod a+x /usr/bin//go-librespot-daemon
 
 echo "[Unit]
 Description = go-librespot Daemon
 After = volumio.service
 
 [Service]
-#ExecStart=/bin/streaming-daemon
-WorkingDirectory=/home/volumio/new-spotify-plugin/librespot-go/go-librespot
-ExecStart=/home/volumio/new-spotify-plugin/librespot-go/go-librespot/run.sh
+ExecStart=/usr/bin/go-librespot-daemon -config_path /tmp/go-librespot-config.yml -credentials_path /data/configuration/music_service/spop/spotifycredentials.json
 Restart=always
 StandardOutput=syslog
 StandardError=syslog
