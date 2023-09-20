3dbd8a8a1bf963bed18db4a7bdea8975a6fd6dfb
Volumio <info@volumio.org>
2023-09-20 13:32:21 +0000

    Fix download

--- install.sh
+++ install.sh
@@ -11,17 +11,18 @@ elif  [ $ARCH = "amd64" ] || [ $ARCH = "x86_64" ] || [ $ARCH = "x64" ]; then
 elif  [ $ARCH = "i386" ] || [ $ARCH = "i686" ] || [ $ARCH = "x86" ]; then
 	echo "Platform not supported" 
         exit 1
-else
+fi
 
 DAEMON_BASE_URL=https://github.com/devgianlu/go-librespot/releases/download/v
 VERSION=0.0.2
 DAEMON_ARCHIVE=go-librespot_linux_$ARCH.tar.gz
 DAEMON_DOWNLOAD_URL=$DAEMON_BASE_URL$VERSION/$DAEMON_ARCHIVE
+DAEMON_DOWNLOAD_PATH=/home/volumio/$DAEMON_ARCHIVE
 
 echo "Dowloading daemon"
-wget $DAEMON_DOWNLOAD_URL -O /home/volumio
-tar xf /home/volumio/$DAEMON_ARCHIVE -C /usr/bin/
-rm /home/volumio/$DAEMON_ARCHIVE
+wget $DAEMON_DOWNLOAD_URL -O $DAEMON_DOWNLOAD_PATH
+tar xf $DAEMON_DOWNLOAD_PATH -C /usr/bin/
+rm $DAEMON_DOWNLOAD_PATH
 chmod a+x /usr/bin/go-librespot
 
 echo "[Unit]
