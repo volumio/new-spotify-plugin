c1d43b068030fdd728ee4cb52700955765d3fd44
Volumio <info@volumio.org>
2023-09-20 13:26:07 +0000

    Install from built artifacts

--- install.sh
+++ install.sh
@@ -4,20 +4,32 @@ echo "Installing Go-librespot"
 
 ARCH=$(cat /etc/os-release | grep ^VOLUMIO_ARCH | tr -d 'VOLUMIO_ARCH="')
 
-DAEMON_BASE_URL=https://repo.volumio.org/Packages/go-librespot-daemon/go-librespot-daemon
-DAEMON_DOWNLOAD_URL=$DAEMON_BASE_URL-$ARCH
+if [ $ARCH = "armv7" ] || [ $ARCH = "armv6" ] || [ $ARCH = "armhf" ] || [ $ARCH = "arm" ]; then
+	ARCH="armv6"
+elif  [ $ARCH = "amd64" ] || [ $ARCH = "x86_64" ] || [ $ARCH = "x64" ]; then
+	ARCH="x86_64"
+elif  [ $ARCH = "i386" ] || [ $ARCH = "i686" ] || [ $ARCH = "x86" ]; then
+	echo "Platform not supported" 
+        exit 1
+else
 
-# TODO DOWNLOAD FROM RELEASES URL
+DAEMON_BASE_URL=https://github.com/devgianlu/go-librespot/releases/download/v
+VERSION=0.0.2
+DAEMON_ARCHIVE=go-librespot_linux_$ARCH.tar.gz
+DAEMON_DOWNLOAD_URL=$DAEMON_BASE_URL$VERSION/$DAEMON_ARCHIVE
+
 echo "Dowloading daemon"
-wget $DAEMON_DOWNLOAD_URL -O /usr/bin/go-librespot-daemon
-chmod a+x /usr/bin//go-librespot-daemon
+wget $DAEMON_DOWNLOAD_URL -O /home/volumio
+tar xf /home/volumio/$DAEMON_ARCHIVE -C /usr/bin/
+rm /home/volumio/$DAEMON_ARCHIVE
+chmod a+x /usr/bin/go-librespot
 
 echo "[Unit]
 Description = go-librespot Daemon
 After = volumio.service
 
 [Service]
-ExecStart=/usr/bin/go-librespot-daemon -config_path /tmp/go-librespot-config.yml -credentials_path /data/configuration/music_service/spop/spotifycredentials.json
+ExecStart=/usr/bin/go-librespot -config_path /tmp/go-librespot-config.yml -credentials_path /data/configuration/music_service/spop/spotifycredentials.json
 Restart=always
 StandardOutput=syslog
 StandardError=syslog
--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,7 @@
 
 echo "Unistalling go-librespot-daemon"
 
-rm /usr/bin/go-librespot-daemon
+rm /usr/bin/go-librespot
 rm /lib/systemd/system/go-librespot-daemon.service
 
 echo "Done"
