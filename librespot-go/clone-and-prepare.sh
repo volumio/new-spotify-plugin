73cb27878622308233423ea95af82e7806238050
Volumio <info@volumio.org>
2023-10-03 18:53:43 +0000

    Compile and deploy

--- librespot-go/clone-and-prepare.sh
+++ librespot-go/clone-and-prepare.sh
@@ -13,3 +13,14 @@ audio_device: "volumio"' > config.yml
 echo "#!/bin/sh
 /usr/local/go/bin/go run ./cmd/daemon -config_path /tmp/go-librespot-config.yml -credentials_path /data/configuration/music_service/spop/spotifycredentials.json" > run.sh
 chmod a+x run.sh
+
+echo "#!/bin/sh
+/usr/local/go/bin/go build  -ldflags="-s -w" ./cmd/daemon" > compile.sh
+chmod a+x compile.sh
+
+echo "#!/bin/sh
+/usr/local/go/bin/go build  -ldflags="-s -w" ./cmd/daemon
+killall go-librespot
+sudo cp -rp daemon /usr/bin/go-librespot
+sudo chmod a+x /usr/bin/go-librespot" > compile-and-deploy.sh
+chmod a+x compile-and-deploy.sh
